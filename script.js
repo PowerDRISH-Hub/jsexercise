7b9318462814cba8464f391e2af2f991d2f16795
islate <darkslate115@gmail.com>
2022-11-16 23:00:59 +0500

    jscodeexercises

---
+++ script.js
@@ -0,0 +1,15 @@
+var name = prompt("Введите своё имя")
+console.log(name);
+var age = +prompt("Введите свой возраст")
+console.log(age);
+var plus = prompt("10 + 5")
+var minus = prompt("10 - 5")
+var umnoj = prompt("10 * 5")
+var delit = prompt("10 / 5")
+var ostat = prompt("10 % 5")
+alert("Зайдите в консоль F12")
+
+
+console.log(" (10 + 5 = 15) " + " Ваш ответ" + " " + plus + "." + " (10 - 5 = 5) " + " Ваш ответ" + " " + minus + "." + " (10 * 5 = 50) " + " Ваш ответ" + " " + umnoj + "." +
+    " (10 / 5 = 2) " + " Ваш ответ" + " " + delit + "." + " (10 % 5 = 0) " + " Ваш ответ" + " " + ostat + ".")
+
